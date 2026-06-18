dd2937ec460438f60773d4ef15d472780d2a4e82
nikitanurainun <nikitanurainunn@gmail.com>
2026-06-18 08:16:40 +0700

    merge

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\AgendaEventController;
-=======
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EKartuController;
->>>>>>> origin/stenly
 use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -46,9 +42,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/agenda/create', [AgendaEventController::class, 'create'])->name('agenda.create');
-    Route::post('/agenda', [AgendaEventController::class, 'store'])->name('agenda.store');
 });
 
 require __DIR__.'/auth.php';
